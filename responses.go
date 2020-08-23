2fa75e3487baf7555f9d6842e1034227a7bf6098
Alex P <alexeek@protonmail.com>
2020-08-23 15:07:02 +0100

    feat: Add Data field to response struct

--- responses.go
+++ responses.go
@@ -7,9 +7,10 @@ const (
 	errorExists
 )
 
-// response is a generic HTTP response. If HTTP is zeroed, Code will be used.
+// response is a generic HTTP response. If HTTP is zeroed, Code should be used.
 type response struct {
-	Code    statusCode `json:"code"`
-	Message string     `json:"message"`
-	HTTP    int        `json:"-"`
+	Code    statusCode  `json:"code"`
+	Message string      `json:"message"`
+	HTTP    int         `json:"-"`
+	Data    interface{} `json:"data"`
 }
