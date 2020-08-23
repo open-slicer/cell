cb0d81cd575009821c68c9315db6afe2f8efcc9b
Alex P <alexeek@protonmail.com>
2020-08-23 16:52:58 +0100

    refactor: Merge util.go with responses.go

--- util.go
+++ util.go
@@ -1,5 +1,34 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
+type statusCode int
+
+const (
+	errorInternalError statusCode = iota
+	errorExists
+	errorPasswordInsecure
+	errorTooLarge
+	errorBindFailed
+	errorMissingField
+)
+
+// response is a generic HTTP response. If HTTP is zeroed, Code should be used.
+type response struct {
+	Code    statusCode  `json:"code"`
+	Message string      `json:"message"`
+	HTTP    int         `json:"-"`
+	Data    interface{} `json:"data"`
+}
+
+func (r response) send(c *gin.Context) {
+	if r.HTTP == 0 {
+		c.JSON(int(r.Code), r)
+		return
+	}
+	c.JSON(r.HTTP, r)
+}
+
 // someZero checks if a single value is zeroed. Supports: string, int.
 func someZero(vals ...interface{}) bool {
 	if vals[0] == nil {
