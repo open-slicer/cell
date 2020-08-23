46a32207204f32037d20b6a15c0aad38a57208ce
Alex P <alexeek@protonmail.com>
2020-08-23 15:02:58 +0100

    docs(godoc): Document response struct

--- responses.go
+++ responses.go
@@ -7,6 +7,7 @@ const (
 	errorExists
 )
 
+// response is a generic HTTP response. If HTTP is zeroed, Code will be used.
 type response struct {
 	Code    statusCode `json:"code"`
 	Message string     `json:"message"`
