ba629cb9d00e2a51bfef000fa4f7039962f79992
Alex P <alexeek@protonmail.com>
2020-08-23 21:27:22 +0100

    feat: Add auth routes

--- routes.go
+++ routes.go
@@ -1,8 +1,16 @@
 package main
 
 func init() {
+	authMiddleware, _ := getAuthMiddleware()
+
 	v2 := r.Group("/api/v2")
 	{
 		v2.POST("/users", handleUsersPost)
+
+		auth := v2.Group("/auth")
+		{
+			auth.POST("/login", authMiddleware.LoginHandler)
+			auth.GET("/refresh", authMiddleware.RefreshHandler)
+		}
 	}
 }
