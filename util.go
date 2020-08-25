03966a33ab5bd0c2cb514af578d397aa448d10cc
Alex P <alexeek@protonmail.com>
2020-08-25 23:17:48 +0100

    refactor: Remove unused functions

--- util.go
+++ util.go
@@ -19,16 +19,8 @@ const (
 	errorPasswordInsecure
 	errorTooLarge
 	errorBindFailed
-	errorMissingField
-	errorDidntMatch
 	errorNotFound
 )
-
-type tooLargeData struct {
-	Offending []string `json:"offending"`
-	Got       int      `json:"got"`
-	Want      int      `json:"want"`
-}
 
 // response is a generic HTTP response. If HTTP is zeroed, Code should be used.
 type response struct {
@@ -44,43 +36,6 @@ func (r response) send(c *gin.Context) {
 		return
 	}
 	c.JSON(r.HTTP, r)
-}
-
-// someZero checks if a single value is zeroed. Supports: string, int.
-func someZero(vals ...interface{}) bool {
-	if vals[0] == nil {
-		return true
-	}
-	check := func(val interface{}) bool {
-		return true
-	}
-
-	switch vals[0].(type) {
-	case string:
-		check = func(val interface{}) bool {
-			if val == "" {
-				return true
-			}
-			return false
-		}
-	case int:
-		check = func(val interface{}) bool {
-			if val == 0 {
-				return true
-			}
-			return false
-		}
-	}
-
-	for _, val := range vals {
-		if val == nil {
-			return true
-		}
-		if check(val) {
-			return true
-		}
-	}
-	return false
 }
 
 type internalErrorData struct {
