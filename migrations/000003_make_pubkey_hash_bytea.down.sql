a4ee1fc6a27c31f50ea851189d8ac1ba4c062b55
Alex P <alexeek@protonmail.com>
2020-08-30 18:42:21 +0100

    fix: Drop table instead of altering

--- migrations/000003_make_pubkey_hash_bytea.down.sql
+++ migrations/000003_make_pubkey_hash_bytea.down.sql
@@ -1,4 +1,10 @@
-ALTER TABLE users
-    ALTER COLUMN password_hash TYPE CHAR(60);
-ALTER TABLE users
-    ALTER COLUMN public_key TYPE TEXT;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users
+(
+    id            CHAR(20) PRIMARY KEY,
+    username      VARCHAR(32) NOT NULL UNIQUE,
+    display_name  VARCHAR(32),
+    password_hash CHAR(60)    NOT NULL,
+    public_key    TEXT        NOT NULL
+);
