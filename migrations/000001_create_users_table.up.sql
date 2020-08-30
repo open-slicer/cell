bbe631c75c85ff76ee8a6736ecb342a66067e9b2
Alex P <alexeek@protonmail.com>
2020-08-30 17:33:30 +0100

    migrate: Add up migration for create_users_table

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id            CHAR(20) PRIMARY KEY,
+    username      VARCHAR(32) NOT NULL UNIQUE,
+    display_name  VARCHAR(32),
+    password_hash CHAR(60)    NOT NULL,
+    public_key    TEXT        NOT NULL
+);
