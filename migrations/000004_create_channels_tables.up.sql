1b802a421de649e1aa3bb88e23c7806b3eabebde
Alex P <alexeek@protonmail.com>
2020-08-30 23:22:29 +0100

    migrate/fix: Add owner column to invites

--- migrations/000004_create_channels_tables.up.sql
+++ migrations/000004_create_channels_tables.up.sql
@@ -10,14 +10,15 @@ CREATE TABLE invites
 (
     id      VARCHAR(20) PRIMARY KEY,
     name    VARCHAR(32) UNIQUE NOT NULL,
-    channel VARCHAR(20)        NOT NULL REFERENCES channels (id) ON DELETE CASCADE
+    channel VARCHAR(20)        NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
+    owner   VARCHAR(20)        NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
 CREATE INDEX ON invites (name, channel);
 
 CREATE TABLE members
 (
     id          VARCHAR(20) PRIMARY KEY REFERENCES users (id) ON DELETE CASCADE,
-    channel     VARCHAR(20) REFERENCES channels (id) ON DELETE CASCADE,
+    channel     VARCHAR(20) NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
     permissions BIGINT
 );
 CREATE INDEX ON members (channel);
