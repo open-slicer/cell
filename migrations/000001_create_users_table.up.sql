e860eee93af6ba5fb1c6484ac6884310e0ea69a8
Alex P <alexeek@protonmail.com>
2020-08-30 19:36:05 +0100

    migrate: Clean up migrations

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE users
     id            CHAR(20) PRIMARY KEY,
     username      VARCHAR(32) NOT NULL UNIQUE,
     display_name  VARCHAR(32),
-    password_hash CHAR(60)    NOT NULL,
-    public_key    TEXT        NOT NULL
+    password_hash BYTEA       NOT NULL,
+    public_key    BYTEA       NOT NULL
 );
