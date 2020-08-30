6e90d1243e0de31f1c42168daa0fd1d86833fd69
Alex P <alexeek@protonmail.com>
2020-08-30 21:25:03 +0100

    migrate: Add create_channels_tables migration

---
+++ migrations/000004_create_channels_tables.up.sql
@@ -0,0 +1,30 @@
+CREATE TABLE channels
+(
+    id     VARCHAR(20) PRIMARY KEY,
+    name   VARCHAR(32) NOT NULL,
+    owner  VARCHAR(20) NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    parent VARCHAR(20) NOT NULL REFERENCES channels (id) ON DELETE CASCADE
+);
+
+CREATE TABLE invites
+(
+    id      VARCHAR(20) PRIMARY KEY,
+    name    VARCHAR(32) UNIQUE NOT NULL,
+    channel VARCHAR(20)        NOT NULL REFERENCES channels (id) ON DELETE CASCADE
+);
+CREATE INDEX ON invites (name, channel);
+
+CREATE TABLE members
+(
+    id          VARCHAR(20) PRIMARY KEY REFERENCES users (id) ON DELETE CASCADE,
+    permissions BIGINT
+);
+
+CREATE TABLE messages
+(
+    id             VARCHAR(20) PRIMARY KEY,
+    content_cipher TEXT        NOT NULL,
+    channel        VARCHAR(20) NOT NULL REFERENCES channels (id) ON DELETE CASCADE,
+    owner          VARCHAR(20) NOT NULL REFERENCES channels (id) ON DELETE CASCADE
+);
+CREATE INDEX ON messages (channel);
