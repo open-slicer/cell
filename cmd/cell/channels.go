6db2e3dd76d38121f1243d7bce47d19c88088862
Alex P <alexeek@protonmail.com>
2020-08-30 23:22:55 +0100

    feat: Complete channelInsertion.insert

--- cmd/cell/channels.go
+++ cmd/cell/channels.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"github.com/jackc/pgx/v4"
+	"net/http"
+)
 
 type channel struct {
 	ID     string `json:"id"`
@@ -15,7 +19,20 @@ type channelInsertion struct {
 }
 
 func (req *channelInsertion) insert(requesterID string) response {
-	if req.Parent == "" {
+	if req.Parent != "" {
+		if _, err := pg.Exec(
+			context.Background(), "SELECT 1 FROM channels WHERE id = $1", req.Parent,
+		); err != nil {
+			if err != pgx.ErrNoRows {
+				return internalError(err)
+			}
+			return response{
+				Code:    errorParentNotExists,
+				Message: "A parent channel with the provided ID doesn't exist",
+				HTTP:    http.StatusBadRequest,
+			}
+		}
+	} else {
 		req.Parent = "origin"
 	}
 
@@ -25,9 +42,23 @@ func (req *channelInsertion) insert(requesterID string) response {
 		Owner:  requesterID,
 		Parent: req.Parent,
 	}
+
+	if _, err := pg.Exec(
+		context.Background(),
+		"INSERT INTO channels (id, name, owner, parent) VALUES ($1, $2, $3, $4)",
+		c.ID, c.Name, c.Owner, c.Parent,
+	); err != nil {
+		return internalError(err)
+	}
+	if _, err := pg.Exec(
+		context.Background(), "INSERT INTO members (id, channel) VALUES ($1, $2)", c.Owner, c.ID,
+	); err != nil {
+		return internalError(err)
+	}
+
 	return response{
 		Code:    http.StatusCreated,
-		Message: "Channel created",
+		Message: "Channel created and member created for owner",
 		Data:    c,
 	}
 }
