5c0285c5a615e7db172f1eeb2a128c90ee736701
Alex P <alexeek@protonmail.com>
2020-08-30 17:52:38 +0100

    feat: Add interrupt listener

--- cmd/cell/main.go
+++ cmd/cell/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"os"
-
 	"github.com/getsentry/sentry-go"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
+	"os"
+	"os/signal"
 )
 
 func main() {
@@ -41,13 +41,23 @@ func main() {
 
 		// Let's assume key_file is present.
 		keyFile := viper.GetString("security.key_file")
-		if err := r.RunTLS(addr, certFile, keyFile); err != nil {
-			log.Fatal().Bool("tls", true).Str("addr", addr).Err(err).Msg("Failed to start HTTP server")
-		}
+		go func() {
+			if err := r.RunTLS(addr, certFile, keyFile); err != nil {
+				log.Fatal().Bool("tls", true).Str("addr", addr).Err(err).Msg("Failed to start HTTP server")
+			}
+		}()
 	}
 
 	log.Info().Bool("tls", false).Str("addr", addr).Msg("Starting HTTP server")
-	if err := r.Run(addr); err != nil {
-		log.Fatal().Err(err).Msg("Starting HTTP server")
-	}
+	go func() {
+		if err := r.Run(addr); err != nil {
+			log.Fatal().Err(err).Msg("Starting HTTP server")
+		}
+	}()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	<-sigs
+
+	log.Info().Msg("Interrupt received, gracefully exiting")
 }
