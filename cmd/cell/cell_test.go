9eafddb177d48dc79f058051a2f58544be6c31e0
Alex P <alexeek@protonmail.com>
2020-08-30 15:58:47 +0100

    feat: Add getExpect

--- cmd/cell/cell_test.go
+++ cmd/cell/cell_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/gavv/httpexpect"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"os"
 	"testing"
 )
@@ -18,4 +20,17 @@ func setup() {
 	readConfig()
 	dbConnect()
 	r = setupRouter()
+}
+
+func getExpect(t *testing.T) *httpexpect.Expect {
+	return httpexpect.WithConfig(httpexpect.Config{
+		Client: &http.Client{
+			Transport: httpexpect.NewBinder(r),
+			Jar:       httpexpect.NewJar(),
+		},
+		Reporter: httpexpect.NewAssertReporter(t),
+		Printers: []httpexpect.Printer{
+			httpexpect.NewDebugPrinter(t, true),
+		},
+	})
 }
